3170fede1847eb54e5539ae07c67b3d382227b2d
Edu <61661507+edu-bio@users.noreply.github.com>
2021-03-11 11:44:03 +0100

    Probando Panache

--- quarkus-database-mariadb/src/main/java/org/acme/resources/LibroResource.java
+++ quarkus-database-mariadb/src/main/java/org/acme/resources/LibroResource.java
@@ -3,8 +3,6 @@ package org.acme.resources;
 import java.net.URI;
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -25,25 +23,69 @@ import org.acme.clases.Libro;
 
 @Path("/libro")
 public class LibroResource {
+	//Asi sería la clase usando EntityManager
+//	@Inject
+//	EntityManager entityManager;
+//	
+//	//Crea un libro en la base de datos
+//	@POST
+//	@Transactional
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response crearLibro(Libro libro) {
+//		entityManager.persist(libro);
+//		return Response.ok(URI.create("/libro/" + libro.getId())).build();
+//	}
+//	
+//	//Devuelve un libro de la base de datos por id
+//	@GET
+//	@Path("{id}")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Libro getLibro(@PathParam("id")Integer id) {
+//		return entityManager.find(Libro.class, id);
+//	}
+//	
+//	//devuelve la lista de libros
+//		@GET
+//	    @Path("all")
+//		@Produces(MediaType.APPLICATION_JSON)
+//	    public List<Libro> getLista() {
+//	        return entityManager.createNamedQuery("Libro.findAll", Libro.class)
+//	                .getResultList();
+//	    }
+//	
+//	//Borra un libro de la base de datos por id
+//	@DELETE
+//	@Transactional
+//	@Path("{id}")
+//	public void borrarLibro(@PathParam("id")Integer id) {
+//		 entityManager.remove(entityManager.find(Libro.class, id));
+//	}
+//	
+//	
+//	@PUT
+//	@Transactional
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public ResponseBuilder actualizarLibro(Libro libro){
+//		entityManager.refresh(libro);
+//		return Response.ok(libro);
+//	}
 	
-	@Inject
-	EntityManager entityManager;
+	//Asi seria usando Panache
 	
-	//Crea un libro en la base de datos
 	@POST
 	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response crearLibro(Libro libro) {
-		entityManager.persist(libro);
-		return Response.ok(URI.create("/libro/" + libro.getId())).build();
+		libro.persist();
+		return Response.ok(URI.create("/libro/" + libro.id)).build();
 	}
 	
 	//Devuelve un libro de la base de datos por id
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Libro getLibro(@PathParam("id")Integer id) {
-		return entityManager.find(Libro.class, id);
+	public Libro getLibro(@PathParam("id")Long id) {
+		return Libro.findById(id);
 	}
 	
 	//devuelve la lista de libros
@@ -51,24 +93,23 @@ public class LibroResource {
 	    @Path("all")
 		@Produces(MediaType.APPLICATION_JSON)
 	    public List<Libro> getLista() {
-	        return entityManager.createNamedQuery("Libro.findAll", Libro.class)
-	                .getResultList();
+	        return Libro.findAll().list();
 	    }
 	
 	//Borra un libro de la base de datos por id
 	@DELETE
 	@Transactional
 	@Path("{id}")
-	public void borrarLibro(@PathParam("id")Integer id) {
-		 entityManager.remove(entityManager.find(Libro.class, id));
+	public void borrarLibro(@PathParam("id")Long id) {
+		 Libro.deleteById(id);
 	}
 	
-	
+	//actualiza un libro en la base de datos
 	@PUT
 	@Transactional
 	@Consumes(MediaType.APPLICATION_JSON)
 	public ResponseBuilder actualizarLibro(Libro libro){
-		entityManager.refresh(libro);
+		libro.flush();
 		return Response.ok(libro);
 	}
 	
